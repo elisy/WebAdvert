74b2832d7a775482258a3a39d30155e3bcb529d9
elisy <ebishkek@yandex.ru>
2020-01-07 22:56:39 +0600

    New .net core project

---
+++ WebAdvert.Web/Models/Accounts/SignupModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAdvert.Web.Models.Accounts
+{
+    public class SignupModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6, ErrorMessage = "Password must be at least 6 characters long")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and conformation do not match")]
+        [Display(Name = "Confirm password")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
